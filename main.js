ea53b014f36039f3fe196c592a44d8ec3f29deef
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-08-18 14:45:46 -0400

    Add woorking list compiler function for movie credits

--- main.js
+++ main.js
@@ -1,216 +1,280 @@
 /*************
-        SAMPLE URLS
-        
-        1. To get the config data like image base urls
-        https://api.themoviedb.org/3/configuration?api_key=<APIKEY>
-        
-        2. To fetch a list of movies based on a keyword
-        https://api.themoviedb.org/3/search/movie?api_key=<APIKEY>&query=<keyword>
-        
-        3. To fetch more details about a movie
-        https://api.themoviedb.org/3/movie/<movie-id>?api_key=<APIKEY>
-        *************/
+SAMPLE URLS
+
+1. To get the config data like image base urls
+https://api.themoviedb.org/3/configuration?api_key=<APIKEY>
+
+2. To fetch a list of movies based on a keyword
+https://api.themoviedb.org/3/search/movie?api_key=<APIKEY>&query=<keyword>
+
+3. To fetch more details about a movie
+https://api.themoviedb.org/3/movie/<movie-id>?api_key=<APIKEY>
+*************/
 //const APIKEY is inside key.js
-
-
 
 const baseURL = "https://api.themoviedb.org/3/";
 
 const APIKEY = apiconfig.API_TOKEN;
 
-
-
-
 const config = "https://api.themoviedb.org/3/configuration?api_key=" + APIKEY;
 
-const search = "https://api.themoviedb.org/3/search/movie?api_key=" + APIKEY + "&query=";
+const search =
+  "https://api.themoviedb.org/3/search/movie?api_key=" + APIKEY + "&query=";
 
 const person = "https://api.themoviedb.org/3/search/person/";
 
+async function talk() {
+  console.log("fetching config");
 
+  const rawData = await fetch(config);
+  const data = await rawData.json();
 
-
-
-async function talk () {
-
-        console.log("fetching config")
-
-        const rawData = await fetch(config)
-        const data = await rawData.json();
-
-        console.log("config data", data);
-        return data;
-
-    
+  console.log("config data", data);
+  return data;
 }
 
 talk();
 
+async function digUp(keyword) {
+  console.log("fetching search for", keyword);
 
-async function digUp (keyword) {
-        console.log("fetching search for", keyword);
+  const rawData = await fetch(search + decodeURI(keyword));
+  const data = await rawData.json();
 
-        const rawData = await fetch(search + decodeURI(keyword));
-        const data = await rawData.json();
+  console.log(data);
+  console.log("Results of data:", data.results);
+  console.log("First returned result is - ", data.results[0]);
 
-        console.log(data);
-        console.log("Results of data:", data.results);
-        console.log("First returned result is - ", data.results[0]);
-        return data;
+  const movieId = data.results[0].id;
+
+  movieCredits(movieId);
+
+  return data;
 }
 
+async function movieCredits(keyword) {
+  console.log("finding movie credits on ID#", keyword);
 
-async function digUpPerson (keyword) {
-        console.log("fetching data on", keyword);
+  const rawData = await fetch(
+    "https://api.themoviedb.org/3/movie/" +
+      keyword +
+      "/credits?api_key=" +
+      APIKEY +
+      "&language=en"
+  );
 
-        const rawData = await fetch(
-            "https://api.themoviedb.org/3/search/person?api_key="
-            + APIKEY
-            + "&language=en-US&query="
-            + decodeURI(keyword)
-            +"&page=1&include_adult=false"
-             );
+  const movieCreditsData = await rawData.json();
 
-        const data = await rawData.json();
+  console.log(movieCreditsData);
 
-        console.log(data);
-        console.log("Results of data:", data.results);
-        console.log("First returned result is - ", data.results[0]);
+  console.log("credit_id is...", movieCreditsData.cast[0].credit_id);
 
-
-        $(".output-box").text(data.results[0]);
-
-        const picAddress = data.results[0].profile_path;
-
-        const portrait = "<img src='http://image.tmdb.org/t/p/w185" + picAddress + "'>";
-
-        console.log(portrait);
-
-        $(".output-box").append(portrait);
-
-        return data;
-
-
-
+  checkCreditId(movieCreditsData.cast[0].credit_id);
 }
 
+async function checkCreditId(keyword) {
+  console.log("finding movie credit data on ID#", keyword);
 
+  const rawData = await fetch(
+    "https://api.themoviedb.org/3/credit/" + keyword + "?api_key=" + APIKEY
+  );
 
+  const whatCreditIsThis = await rawData.json();
 
+  console.log("credit_id#", keyword, "is listed as:", whatCreditIsThis);
+}
 
+async function digUpPerson(keyword) {
+  console.log("fetching data on", keyword);
 
+  const rawData = await fetch(
+    "https://api.themoviedb.org/3/search/person?api_key=" +
+      APIKEY +
+      "&language=en-US&query=" +
+      decodeURI(keyword) +
+      "&page=1&include_adult=false"
+  );
 
+  const data = await rawData.json();
 
+  console.log(data);
+  console.log("Results of data:", data.results);
+  console.log("First returned result is - ", data.results[0]);
+  console.log("First returned result, 'id' is", data.results[0].id);
+
+  $(".output-box").text(data.results[0]);
+
+  const picAddress = data.results[0].profile_path;
+
+  const portrait =
+    "<img src='http://image.tmdb.org/t/p/w185" + picAddress + "'>";
+
+  console.log(portrait);
+
+  $(".output-box").append(portrait);
+
+  return data;
+}
+
+//
+//
+async function finder(keyword) {
+  //
+  //
+  console.log("finding data on", keyword);
+
+  const rawData = await fetch(
+    "https://api.themoviedb.org/3/search/person?api_key=" +
+      APIKEY +
+      "&language=en-US&query=" +
+      decodeURI(keyword) +
+      "&page=1&include_adult=false"
+  );
+
+  const data = await rawData.json();
+
+  console.log(data);
+
+  //
+  //
+  const actorId = data.results[0].id;
+
+  console.log("fetching actor by id#", actorId);
+
+  actorById(actorId);
+
+  return data;
+}
+
+async function actorById(actorId) {
+  const actor = await fetch(
+    "https://api.themoviedb.org/3/person/" +
+      actorId +
+      "?api_key=" +
+      APIKEY +
+      "&append_to_response=movie_credits"
+  );
+
+  const actorData = await actor.json();
+
+  console.log("See below for actor data");
+  console.log(actorData);
+}
+
+//
+//
 
 $(".submit-button").on("click", () => {
-    let userGuess = $(".text-box").val();
-    console.log("The user has entered", userGuess);
-    digUp(userGuess);
-})
-
+  let userGuess = $(".text-box").val();
+  console.log("The user has entered", userGuess);
+  digUp(userGuess);
+});
 
 $(".submit-button-actor").on("click", () => {
-    let userGuess = $(".text-box-person").val();
-    console.log("The user has entered", userGuess);
+  let userGuess = $(".text-box-person").val();
+  console.log("The user has entered", userGuess);
 
-    let returnData = digUpPerson(userGuess);
+  let returnData = digUpPerson(userGuess);
 
-    console.log("inside event listener, we got this back on a person search", returnData);
+  console.log("inside actor event listener,", returnData);
+});
 
-    
+$(".submit-button-finder").on("click", () => {
+  let userGuess = $(".text-box-finder").val();
+  console.log("The user has entered", userGuess);
 
-})
+  let returnData = finder(userGuess);
 
+  console.log("inside finder event listener,", returnData);
+});
 
+async function getAllActingCredits(actor) {
+  console.log("Searching TMDB for: ", actor);
 
+  const rawSearchForActorByName = await fetch(
+    "https://api.themoviedb.org/3/search/person?api_key=" +
+      APIKEY +
+      "&language=en-US&query=" +
+      decodeURI(actor) +
+      "&page=1&include_adult=false"
+  );
 
+  const searchForActorByName = await rawSearchForActorByName.json();
 
+  console.log("Received the following data:", searchForActorByName);
 
-// let configData = null;
+  console.log("First result is:", searchForActorByName.results[0]);
 
-// let baseImageURL = null;
+  const firstId = searchForActorByName.results[0].id;
 
-// let getConfig = function () {
-//   let url = "".concat(baseURL, "configuration?api_key=", APIKEY);
-//   fetch(url)
-//     .then((result) => {
-//       return result.json();
-//     })
-//     .then((data) => {
-//       baseImageURL = data.images.secure_base_url;
-//       configData = data.images;
-//       console.log("config:", data);
-//       console.log("config fetched");
-//       runSearch("jaws");
-//     })
-//     .catch(function (err) {
-//       alert(err);
-//     });
-// };
+  console.log("First result id# is:", firstId);
 
-// let runSearch = function (keyword) {
-//   let url = "".concat(
-//     baseURL,
-//     "search/movie?api_key=",
-//     APIKEY,
-//     "&query=",
-//     keyword
-//   );
-//   fetch(url)
-//     .then((result) => result.json())
-//     .then((data) => {
-//       //process the returned data
-//       document.getElementById("output").innerHTML = JSON.stringify(
-//         data,
-//         null,
-//         4
-//       );
-//       //work with results array...
-//     });
-// };
+  getAllActingCreditsB(firstId, actor);
+}
 
-// getConfig();
+let baconList = "";
 
-// document.addEventListener('DOMContentLoaded', getConfig);
-/*******************************
-        SAMPLE SEARCH RESULTS DATA
-        { "vote_count": 2762, 
-            "id": 578, 
-            "video": false, 
-            "vote_average": 7.5, 
-            "title": "Jaws", 
-            "popularity": 16.273358, 
-            "poster_path": "/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg", 
-            "original_language": "en", 
-            "original_title": "Jaws", 
-            "genre_ids": [ 27, 53, 12 ], 
-            "backdrop_path": "/slkPgAt1IQgxZXNrazEcOzhAK8f.jpg", 
-            "adult": false, 
-            "overview": "An insatiable great white shark terrorizes the townspeople of Amity Island, The police chief, an oceanographer and a grizzled shark hunter seek to destroy the bloodthirsty beast.", 
-            "release_date": "1975-06-18" 
-        }
-        *******************************/
+async function getAllActingCreditsB(firstId, actor) {
+  console.log("Retrieving data on actor:", actor, "with id#:", firstId);
 
-// console.log("Hello there");
+  const rawActingCredits = await fetch(
+    "https://api.themoviedb.org/3/person/" +
+      firstId +
+      "?api_key=" +
+      APIKEY +
+      "&append_to_response=movie_credits"
+  );
 
-// let getAliens = function () {
-//     let url = "".concat(baseURL, "search/movie?api_key=", APIKEY, "&language=en-US&query=aliens&page=1&include_adult=false");
-//     fetch(url)
-//       .then((result) => {
-//         return result.json();
-//       })
-//       .then((data) => {
-//         // baseImageURL = data.images.secure_base_url;
-//         // configData = data.images;
-//         console.log("search:", data);
-//         console.log("aliens fetched");
-//         runSearch("aliens");
-//       })
-//       .catch(function (err) {
-//         alert(err);
-//       });
-//   };
+  const actingCredits = await rawActingCredits.json();
 
+  console.log("Received the following data:", actingCredits);
 
-  
-// getAliens();
+  console.log(actor, "all credits:", actingCredits.movie_credits.cast);
+
+  baconList = actingCredits.movie_credits.cast;
+
+  console.log("baconList is:", baconList);
+
+  cleanCredits(baconList);
+}
+
+getAllActingCredits("Kevin Bacon");
+
+let cleanCreditList = [];
+
+function cleanCredits(creditsArray) {
+  let rawCreditList = [];
+
+  for (const creditObj of creditsArray) {
+    // console.log("This credit is:", credit);
+    //
+    //
+    //here is what we do. we have a list of all the credits.
+    //we want to iterate through all credits, BEFORE, adding to official list.
+    //filter out all music, documentaries and anything else not applicable. We will hopefully have a list
+    //of movies that he has only acted in.
+    //
+    //From there, find every ector he has every worked with. add it to an array called 1st relation.
+    //Do this until we have a list, 6 deep, of potentially every single person who is a possible match
+    //for the game.
+    //we can use this as a filter, so players cannot enter names? maybe an option.
+
+    rawCreditList.push(creditObj);
+  }
+
+  console.log(rawCreditList);
+}
+
+function checkGenres(genre) {
+  if (genre === 99) {
+    return false;
+  }
+
+  if (genre === 10770) {
+    return false;
+  }
+
+  if (genre === 10402) {
+    return false;
+  }
+}
